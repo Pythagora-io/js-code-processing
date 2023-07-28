2d86aebc913db6d808217e1cad7ba67120dd4ca1
pavel-pythagora <pavel@pythagora.ai>
2023-07-28 16:31:02 +0300

    Fix exported variables naming

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const UnitTests = require("./helpers/unitTests");
 const API = require("./helpers/apiClass");
-const codeHelper = require("./utils/code");
-const filesHelper = require("./utils/files");
-const commonHelper = require("./utils/common");
+const codeUtils = require("./utils/code");
+const filesUtils = require("./utils/files");
+const commonUtils = require("./utils/common");
 const colors = require("./const/colors");
 const common = require("./const/common");
 
 module.exports = {
-  codeHelper,
-  filesHelper,
-  commonHelper,
+  codeUtils,
+  filesUtils,
+  commonUtils,
   colors,
   common,
   UnitTests,
